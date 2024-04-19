a7185ee38ad2fa1072d58864770a9800911803a8
Nestor Garcia <nestorisok@hotmail.com>
2024-04-19 13:01:55 -0700

    testing

--- windows32/example.asm
+++ windows32/example.asm
@@ -5,8 +5,6 @@
 .586
 .MODEL FLAT
 
-INCLUDE io.h            ; header file for input/output
-
 .STACK 4096
 
 .DATA
@@ -17,30 +15,38 @@ result DWORD ?
 .CODE
 _MainProc PROC
 
-mov EBX, n
-call myFac
+mov EAX, n
+mov EBX, r
 
-myFac PROC
-push EBP
-mov EBP, ESP
-push EBX
-mov EAX, EBX
+MyPerm PROC
+push EBP		; begin stack
+mov EBP, ESP	; getting stack itself
+push EBX		; pushes r [EBP + 8]
+push EAX		; pushes n [EBP + 12]
 
-FAC:
-cmp EBX, 1
+inFac:
+cmp EAX, 1
 jle LEND
-dec EBX
-imul EAX, EBX
-cmp EBX, 0
-jnz FAC
+dec EAX
+imul EAX, DWORD PTR[EBP + 8]
+loop inFac
+
+
+sub EAX, EBX	; EAX now contains n - r
+cmp EAX, 0		; if EAX is 0, we leave the stack
+jz LEND
+
+
+
 
 LEND:
+pop EAX
 pop EBX
 mov EBP, ESP
 pop EBP
 ret
 
-myFac ENDP
+MyPerm PROC
 
 
 _MainProc ENDP
